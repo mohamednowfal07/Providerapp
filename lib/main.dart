85ca2dc93ea5a3a9f6caf0446163fa22fcd6b420
mohamednowfal07 <nowfal161203@gmail.com>
2025-07-04 19:00:56 +0530

    counter provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: dead_code
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_app/provider/counter_provider.dart';
+import 'package:provider_app/theme_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,25 +12,35 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CounterProvider()),
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ],
+      child: Consumer<ThemeProvider>(builder: (context, theme, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          themeMode: theme.currentTheme,
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.grey,
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primarySwatch: Colors.grey,
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          debugShowCheckedModeBanner: false,
+        );
+      }),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  
 
   final String title;
 
@@ -34,46 +49,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    
+    final counter = Provider.of<CounterProvider>(context);
+    final ThemeMode = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
+        actions: [
+          Switch(
+              value: ThemeMode.isDarkTheme,
+              onChanged: (_) {
+                ThemeMode.toggleTheme();
+              })
+        ],
       ),
       body: Center(
-        
         child: Column(
-        
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '${counter.count}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: counter.increment,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          SizedBox(width: 20),
+          FloatingActionButton(
+            onPressed: counter.decrement,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+          SizedBox(width: 20),
+          FloatingActionButton(
+            onPressed: counter.reset,
+            tooltip: 'Reset',
+            child: const Icon(Icons.refresh),
+          ),
+          SizedBox(width: 20),
+        ],
+      ),
     );
   }
 }
